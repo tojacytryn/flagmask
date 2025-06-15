5efcf803e2cbb93189ccbe4e570d49691e48af47
Cytryn <tojacytryn@gmail.com>
2025-06-15 21:18:56 +0200

    1.0.7
    refactor: remove unused flagmask.js and clean up index.js
    
    - Deleted the unused flagmask.js file.
    - Removed unnecessary comments and the CSS import from index.js.
    - Updated package.json to reflect the removal of flagmask.js and cleaned up formatting.

--- flagmask.js
+++
@@ -1,3 +0,0 @@
-import './flagmask.css';
-
-export default {};
--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// FlagMask - CSS Flag Masking Library
-// Automatically imports the CSS when the package is imported
-
 import './flagmask.css';
 
-// Export an empty object to make this a valid module
 export default {};
